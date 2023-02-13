932e067763219b09cd496fc61718927b350606d2
avinash84319 <avichinnu22@gmail.com>
2023-02-13 09:53:13 +0530

    added react elements

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,40 @@
 import React ,{useEffect ,useState} from 'react';
 import './App.css';
 import Result from './Result'
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
 
 function Start(){
   return <div className='main'>
   <h1 className='mainh1'>Find or Become One Traveler :-</h1>
+  <Box
+      component="form"
+      sx={{
+        '& .MuiTextField-root': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
   <form method="post"  action="http://localhost:5000/addreq" className='formclass'>
-  <input type="text" placeholder="Username" name="username" className="inputs"></input>
-  <input type="text" placeholder="phone no" name="phoneno" className="inputs"></input>
-    <input type="text" placeholder="From" name="from" className="inputs"></input>
-    <input type="text" placeholder="To" name="to" className="inputs" ></input>
-    <input type="date" placeholder="DD/MM/YYYY" name="date" className="inputs"></input>
-    <input type="time" name="time" required className="inputs"></input>
-    <label for="cars" className='inputs'>choose a category :</label>
-  <select id="cars" name="category" className='inputs'>
-  <option value="pp">Pass a package</option>
-  <option value="tr">Traveling</option>
-  <option value="wt">Want to travel</option>
-</select>
-<input type="textbox" name="message" placeholder='message' className='inputs'></input>
-    <button type="submit" className="inputs">✅</button>
+  <TextField id="outlined-required" type="text" placeholder="Username" name="username" className="inputs" ></TextField>
+  <TextField id="outlined-required" type="text" placeholder="phone no" name="phoneno" className="inputs"></TextField>
+    <TextField id="outlined-required" type="text" placeholder="From" name="from" className="inputs"></TextField>
+    <TextField id="outlined-required" type="text" placeholder="To" name="to" className="inputs" ></TextField>
+    <TextField id="outlined-required" type="date" placeholder="DD/MM/YYYY" name="date" className="inputs"></TextField>
+    <TextField id="outlined-required" type="time" name="time" required className="inputs"></TextField>
+<Select placeholder='cat' id="demo-simple-select" name="category">
+  <MenuItem value="pp">Pass a package</MenuItem>
+  <MenuItem value="tr">Traveling</MenuItem>
+  <MenuItem value="wt">Want to travel</MenuItem>
+</Select>
+<TextField id="outlined-required" type="textbox" name="message" placeholder='message' className='inputs'></TextField>
+    <button type="submit" className="sb">SUBMIT</button>
     </form>
+    </Box>
     <Result />
   </div>
 }
